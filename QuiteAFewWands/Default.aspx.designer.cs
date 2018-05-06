09c2aa8d6f8004f4a6c414d615eac81636f36989
Lauren Woods <laurenwoods8@gmail.com>
2018-05-05 21:04:08 -0400

    test message

--- QuiteAFewWands/Default.aspx.designer.cs
+++ QuiteAFewWands/Default.aspx.designer.cs
@@ -101,5 +101,14 @@ namespace QuiteAFewWands {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView ProductGrid;
+        
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
     }
 }
